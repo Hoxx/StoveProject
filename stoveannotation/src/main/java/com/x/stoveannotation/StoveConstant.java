27c228e484816970fa32bd969dd8ecb19f77ee34
Hxx-Company <1416942919@qq.com>
2018-07-02 10:34:00 +0800

    统一管理注解框架类引入与文件生成常量

---
+++ stoveannotation/src/main/java/com/x/stoveannotation/StoveConstant.java
@@ -0,0 +1,17 @@
+package com.x.stoveannotation;
+
+public final class StoveConstant {
+
+    public final static String FindIdGenerateFileName = "$$StoveInject";
+    public final static String FindIdGenerateFilePackageName = "com.x.stoveinject.generate";
+    public final static String FindIdInterfacePackageName = "com.x.stoveinject";
+    public final static String FindIdInterfaceName = "StoveFindIdInterface";
+
+
+    public final static String RouterGenerateFileName = "$$StoveRouterRule";
+    public final static String RouterGenerateFilePackageName = "com.x.stoverouter.generate";
+    public final static String RouterInterfacePackageName = "com.x.stoverouter";
+    public final static String RouterInterfaceName = "StoveRuleInterface";
+
+
+}
